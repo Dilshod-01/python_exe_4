cd78768f632603044197c93e8cad8978671e758c
Dilshod-01 <valiyevdilshod99@gmail.com>
2021-05-11 15:42:24 +0500

    exercise4

---
+++ exercise_4.py
@@ -0,0 +1,6 @@
+def favorite_city(name):
+    print("one of my favorite cities is", name)
+
+favorite_city("Santa Barbara, California")
+favorite_city("Ashaville, north calorina")
+favorite_city("Amsterdam, The Netherlands")    
